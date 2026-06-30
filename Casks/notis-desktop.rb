dac5874e17827405f486b9ef5b5fa1867f72ff06
notis-ci <ci@notis.io>
2026-06-30 15:19:42 +0000

    notis-desktop 0.1.8

--- Casks/notis-desktop.rb
+++ Casks/notis-desktop.rb
@@ -1,13 +1,13 @@
 cask "notis-desktop" do
-  version "0.1.7"
+  version "0.1.8"
 
   on_arm do
-    sha256 "82e2b31df51469d4831b7efb4ea10836ea4198b0863e4b39c9f196871aa56b02"
-    url "https://github.com/notisio/notis-cli/releases/download/desktop-v0.1.7/Notis-0.1.7-arm64.dmg"
+    sha256 "41e0ed24778dbf14f723d0a9b02437d7f8de88e7f48fc73d0e667af8017d9a51"
+    url "https://github.com/notisio/notis-cli/releases/download/desktop-v0.1.8/Notis-0.1.8-arm64.dmg"
   end
   on_intel do
-    sha256 "22da2f1f2a1599c48c94117ce9a9d497c82219d7f90950d8992d9e0a96977c5d"
-    url "https://github.com/notisio/notis-cli/releases/download/desktop-v0.1.7/Notis-0.1.7-x64.dmg"
+    sha256 "8fa17cca6bb05fbea5f39596e0fa15a7dd64a89535355e0414020eb374fd9805"
+    url "https://github.com/notisio/notis-cli/releases/download/desktop-v0.1.8/Notis-0.1.8-x64.dmg"
   end
 
   name "Notis"
